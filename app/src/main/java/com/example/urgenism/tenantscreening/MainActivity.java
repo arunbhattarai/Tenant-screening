79a5fecc2456e90f6ee4ea774c69f47867a91455
arunb <arunbhattarai41@gmail.com>
2023-03-10 23:52:06 +0545

    Import optimization

--- app/src/main/java/com/example/urgenism/tenantscreening/MainActivity.java
+++ app/src/main/java/com/example/urgenism/tenantscreening/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.urgenism.tenantscreening;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
